19d4d836a97a6b0414ceff45ac7f62e376c48872
MetroHege <113589548+MetroHege@users.noreply.github.com>
2023-11-12 17:37:31 +0200

    lunch page

---
+++ src/slideshow.js
@@ -0,0 +1,58 @@
+// TO BE DELETED BEFORE SUBMISSION
+// ONLY WORKS AS JS FILE
+
+var timeOut = 0;
+var slideIndex = 0;
+var autoOn = true;
+
+autoSlides();
+
+function autoSlides() {
+  timeOut = timeOut - 20;
+
+  if (autoOn == true && timeOut < 0) {
+    showSlides();
+  }
+  setTimeout(autoSlides, 20);
+}
+
+function prevSlide() {
+  timeOut = 2000;
+
+  var slides = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("dot");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slideIndex--;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  if (slideIndex == 0) {
+    slideIndex = 3;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+function showSlides() {
+  timeOut = 9000;
+
+  var slides = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("dot");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
