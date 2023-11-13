8246a80a824d51a6184746284688f429fa1d0140
Elina <sankilaelina@gmail.com>
2023-11-13 16:20:33 +0200

    nav bar korjauksia yms

--- src/slideshow.js
+++ src/slideshow.js
@@ -19,12 +19,12 @@ function autoSlides() {
 function prevSlide() {
   timeOut = 2000;
 
-  var slides = document.getElementsByClassName("slides");
-  var dots = document.getElementsByClassName("dot");
+  var slides = document.getElementsByClassName('slides');
+  var dots = document.getElementsByClassName('dot');
 
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-    dots[i].className = dots[i].className.replace(" active", "");
+    slides[i].style.display = 'none';
+    dots[i].className = dots[i].className.replace(' active', '');
   }
   slideIndex--;
 
@@ -34,25 +34,25 @@ function prevSlide() {
   if (slideIndex == 0) {
     slideIndex = 3;
   }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' active';
 }
 
 function showSlides() {
-  timeOut = 9000;
+  timeOut = 3000;
 
-  var slides = document.getElementsByClassName("slides");
-  var dots = document.getElementsByClassName("dot");
+  var slides = document.getElementsByClassName('slides');
+  var dots = document.getElementsByClassName('dot');
 
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-    dots[i].className = dots[i].className.replace(" active", "");
+    slides[i].style.display = 'none';
+    dots[i].className = dots[i].className.replace(' active', '');
   }
   slideIndex++;
 
   if (slideIndex > slides.length) {
     slideIndex = 1;
   }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' active';
 }
