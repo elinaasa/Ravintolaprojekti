1ae71e8499e07466101cc0b22200a3fe90fd5fe4
MetroHege <113589548+MetroHege@users.noreply.github.com>
2023-12-05 13:59:07 +0200

    admin page working

--- Frontend/src/adminPanel.js
+++ Frontend/src/adminPanel.js
@@ -1,7 +1,7 @@
 // Function to fetch menu data from the server
 const getMenuData = async () => {
   try {
-    const response = await fetch("http://localhost:3000/menu");
+    const response = await fetch("http://localhost:3000/api/menu");
     const data = await response.json();
     return data;
   } catch (error) {
@@ -13,9 +13,15 @@ const getMenuData = async () => {
 // Function to update menu data on the server
 const updateMenuData = async (data) => {
   try {
-    const response = await fetch("http://localhost:3000/menu", {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      alert("You must be logged in to update menu data");
+      return;
+    }
+    const response = await fetch("http://localhost:3000/api/menu/" + data.id, {
       method: "PUT",
       headers: {
+        Authorization: "Bearer " + token,
         "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
@@ -29,54 +35,96 @@ const updateMenuData = async (data) => {
 };
 
 // Function to display the menus
+
 const displayMenu = async () => {
-  try {
-    const menuData = await getMenuData();
-    const menuContainer = document.querySelector(".menu-container");
-    if (menuContainer) {
-      menuContainer.innerHTML = "";
-    }
-    menuData.forEach((menu) => {
-      if (menuContainer) {
-        const menuCard = document.createElement("div");
-        menuCard.classList.add("menu-card");
-        menuCard.innerHTML = `
-            <h3>${menu.name}</h3>
-            <p>${menu.description}</p>
-            <p>${menu.diet}</p>
-            <p>${menu.week_number}</p>
-            <p>${menu.day_of_week}</p>
-          `;
-        menuContainer.appendChild(menuCard);
-      }
-    });
-  } catch (error) {
-    console.error("Error displaying menu:", error);
+  const menuData = await getMenuData();
+  const menuContainer = document.querySelector(".menu-container");
+  if (menuContainer) {
+    menuContainer.innerHTML = "";
   }
+  menuData.forEach((menu) => {
+    console.log(menu);
+    if (menuContainer) {
+      const menuCard = document.createElement("div");
+      menuCard.classList.add("menu-card");
+      const h3 = document.createElement("h3");
+      h3.innerHTML = menu.name;
+      const p1 = document.createElement("p");
+      p1.innerHTML = menu.description;
+      const p2 = document.createElement("p");
+      p2.innerHTML = menu.diet;
+      const p3 = document.createElement("p");
+      p3.innerHTML = `Menu week: ${menu.week_number}`;
+      const p4 = document.createElement("p");
+      p4.innerHTML = `Menu day: ${menu.day_of_week}`;
+      const form = document.createElement("form");
+      const input1 = document.createElement("input");
+      input1.setAttribute("type", "text");
+      input1.placeholder = "Name";
+      const input2 = document.createElement("input");
+      input2.setAttribute("type", "text");
+      input2.placeholder = "Description";
+      const input3 = document.createElement("input");
+      input3.setAttribute("type", "text");
+      input3.placeholder = "Diet";
+      const button = document.createElement("button");
+      button.setAttribute("type", "submit");
+      button.innerHTML = "Save Menu";
+      menuCard.appendChild(h3);
+      menuCard.appendChild(p1);
+      menuCard.appendChild(p2);
+      menuCard.appendChild(p3);
+      menuCard.appendChild(p4);
+      menuCard.appendChild(form);
+      form.appendChild(input1);
+      form.appendChild(input2);
+      form.appendChild(input3);
+      form.appendChild(button);
+      form.addEventListener("submit", async (event) => {
+        event.preventDefault();
+        const newMenu = {
+          id: menu.menu_id,
+          name: input1.value,
+          description: input2.value,
+          diet: input3.value,
+          week_number: menu.week_number,
+          day_of_week: menu.day_of_week,
+        };
+        await updateMenuData(newMenu);
+        // menuCard.remove();
+        displayMenu();
+      });
+      menuContainer.appendChild(menuCard);
+    }
+  });
 };
 
 // Function to save the menus
-const saveMenu = async () => {
+// check the id of the menu and update the menu data
+// remove the menu card and display the updated menu
+// save the menu data to the server once the submit button is clicked
+
+const showMenu = async () => {
   const menuName = document.querySelector("#menu-name");
   const menuDescription = document.querySelector("#menu-description");
   const menuDiet = document.querySelector("#menu-diet");
   const menuWeekNumber = document.querySelector("#menu-week-number");
   const menuDayOfWeek = document.querySelector("#menu-day-of-week");
-
   try {
     const menuData = await getMenuData();
-    const newMenu = {
-      id: menuData.length + 1,
-      name: menuName.value,
-      description: menuDescription.value,
-      diet: menuDiet.value,
-      week_number: parseInt(menuWeekNumber.value),
-      day_of_week: parseInt(menuDayOfWeek.value),
-    };
-    menuData.push(newMenu);
-    await updateMenuData(menuData);
+    // const newMenu = {
+    //   id: menuData.length + 1,
+    //   name: menuName.value,
+    //   description: menuDescription.value,
+    //   diet: menuDiet.value,
+    //   week_number: parseInt(menuWeekNumber.value),
+    //   day_of_week: parseInt(menuDayOfWeek.value),
+    // };
+    // menuData.push(newMenu);
     displayMenu();
   } catch (error) {
     console.error("Error saving menu:", error);
   }
 };
+
+displayMenu();
--- Frontend/src/adminPanel.ts
+++ Frontend/src/adminPanel.ts
@@ -10,7 +10,7 @@ interface MenuItem {
 // Function to fetch menu data from the server
 
 const getMenuData = async () => {
-    const response = await fetch('http://localhost:3000/menu');
+    const response = await fetch('http://localhost:3000/api/menu');
     const data = await response.json();
     return data;
   };
@@ -18,7 +18,7 @@ const getMenuData = async () => {
   // function to update menu data on the server on the html page
 
   const updateMenuData = async (data: MenuItem[]) => {
-    const response = await fetch('http://localhost:3000/menu', {
+    const response = await fetch('http://localhost:3000/api/menu', {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
@@ -30,28 +30,37 @@ const getMenuData = async () => {
   };
 
   // Function to display the menus
+  // add input fields for each menu-container to the html page to update the menu
+  // add a button to save the menu
+  
+  const displayMenu = async () => {
+    const menuData = await getMenuData();
+    const menuContainer = document.querySelector('.menu-container');
+    if (menuContainer) {
+      menuContainer.innerHTML = '';
+    }
 
-    const displayMenu = async () => {
-        const menuData = await getMenuData();
-        const menuContainer = document.querySelector('.menu-container');
-        if (menuContainer) {
-            menuContainer.innerHTML = '';
-        }
-        menuData.forEach((menu: MenuItem) => {
-            if (menuContainer) {
-                const menuCard = document.createElement('div');
-                menuCard.classList.add('menu-card');
-                menuCard.innerHTML = `
-                    <h3>${menu.name}</h3>
-                    <p>${menu.description}</p>
-                    <p>${menu.diet}</p>
-                    <p>${menu.week_number}</p>
-                    <p>${menu.day_of_week}</p>
-                `;
-                menuContainer.appendChild(menuCard);
-            }
-        });
-    };
+    menuData.forEach((menu: MenuItem) => {
+      if (menuContainer) {
+        const menuCard = document.createElement('div');
+        menuCard.classList.add('menu-card');
+        menuCard.innerHTML = `
+          <h3>${menu.name}</h3>
+          <p>${menu.description}</p>
+          <p>${menu.diet}</p>
+          <p>${menu.week_number}</p>
+          <p>${menu.day_of_week}</p>
+          <input type="text" id="menu-name" placeholder="Name" />
+          <input type="text" id="menu-description" placeholder="Description" />
+          <input type="text" id="menu-diet" placeholder="Diet" />
+          <input type="text" id="menu-week-number" placeholder="Week Number" />
+          <input type="text" id="menu-day-of-week" placeholder="Day of Week" />
+          <button id="save-menu">Save Menu</button>
+        `;
+        menuContainer.appendChild(menuCard);
+      }
+    });
+  };
 
     // Function to save the menus
 const saveMenu = async () => {
