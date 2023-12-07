00a6d60b230e688226012df10b84a0849337edc9
MetroHege <113589548+MetroHege@users.noreply.github.com>
2023-12-07 09:28:01 +0200

    a lot of small changes

--- Backend/src/routes/menu-router.mjs
+++ Backend/src/routes/menu-router.mjs
@@ -12,6 +12,124 @@ import {body} from 'express-validator';
 
 const menuRouter = express.Router();
 
+/**
+ * @api {get} /menus/ Get All Menus
+ * @apiGroup Menus
+ * @apiDescription Retrieves a list of all menus.
+ * @apiName GetAllMenus
+ *
+ * @apiSuccess {Object[]} data List of menu items.
+ * @apiSuccessExample {json} Success-Response:
+ *    HTTP/1.1 200 OK
+ *    [
+ *      {
+ *        "menu_id": 1,
+ *        "name": "Menu 1",
+ *        "description": "Description of Menu 1",
+ *        "diet": "Vegetarian"
+ *      },
+ *      {
+ *        "menu_id": 2,
+ *        "name": "Menu 2",
+ *        "description": "Description of Menu 2",
+ *        "diet": "Vegan"
+ *      }
+ *    ]
+ */
+
+/**
+ * @api {get} /menus/:menu_id Get Menu by ID
+ * @apiGroup Menus
+ * @apiDescription Retrieves a specific menu by its ID.
+ * @apiName GetMenuByID
+ *
+ * @apiParam {Number} menu_id Unique identifier of the menu.
+ *
+ * @apiSuccess {Object} data Specific menu item.
+ * @apiSuccessExample {json} Success-Response:
+ *    HTTP/1.1 200 OK
+ *    {
+ *      "menu_id": 1,
+ *      "name": "Menu 1",
+ *      "description": "Description of Menu 1",
+ *      "diet": "Vegetarian"
+ *    }
+ *
+ * @apiError 404 Menu not found.
+ * @apiErrorExample {json} Error-Response:
+ *    HTTP/1.1 404 Not Found
+ *    {
+ *      "error": "Menu not found."
+ *    }
+ */
+
+/**
+ * @api {post} /menus/ Add New Menu
+ * @apiGroup Menus
+ * @apiDescription Adds a new menu.
+ * @apiName AddNewMenu
+ *
+ * @apiParam {String} name Menu's name (minimum length: 3).
+ * @apiParam {String} description Menu's description (maximum length: 255).
+ * @apiParam {String} diet Menu's diet type (minimum length: 3).
+ *
+ * @apiSuccess {String} message Message confirming the addition.
+ * @apiSuccess {Number} menu_id Unique identifier of the new menu.
+ * @apiSuccessExample {json} Success-Response:
+ *    HTTP/1.1 201 Created
+ *    {
+ *      "message": "New menu added.",
+ *      "menu_id": 12
+ *    }
+ *
+ * @apiError 400 Bad Request if validation fails.
+ * @apiErrorExample {json} Error-Response:
+ *    HTTP/1.1 400 Bad Request
+ *    {
+ *      "error": "Validation failed."
+ *    }
+ */
+
+/**
+ * @api {put} /menus/:menu_id Update Menu
+ * @apiGroup Menus
+ * @apiDescription Updates an existing menu by its ID.
+ * @apiName UpdateMenu
+ *
+ * @apiParam {Number} menu_id Unique identifier of the menu.
+ * @apiParam {String} name Menu's name (minimum length: 3).
+ * @apiParam {String} description Menu's description (maximum length: 255).
+ * @apiParam {String} diet Menu's diet type (minimum length: 3).
+ *
+ * @apiSuccess {String} message Message confirming the update.
+ * @apiSuccessExample {json} Success-Response:
+ *    HTTP/1.1 200 OK
+ *    {
+ *      "message": "Menu updated."
+ *    }
+ *
+ * @apiError 400 Bad Request if validation fails.
+ * @apiError 404 Not Found if the menu does not exist.
+ */
+
+/**
+ * @api {delete} /menus/:menu_id Delete Menu
+ * @apiGroup Menus
+ * @apiDescription Deletes a specific menu by its ID.
+ * @apiName DeleteMenu
+ *
+ * @apiParam {Number} menu_id Unique identifier of the menu.
+ *
+ * @apiSuccess {String} message Message confirming the deletion.
+ * @apiSuccessExample {json} Success-Response:
+ *    HTTP/1.1 200 OK
+ *    {
+ *      "message": "Menu deleted."
+ *    }
+ *
+ * @apiError 404 Not Found if the menu does not exist.
+ */
+
 menuRouter
   .route('/')
   .get(getMenus)
--- Frontend/src/adminPanel.ts
+++ Frontend/src/adminPanel.ts
@@ -1,84 +1,130 @@
-interface MenuItem {
-    id: number;
-    name: string;
-    description: string | null;
-    diet: string;
-    week_number: number;
-    day_of_week: number;
-  }
+interface Menu {
+  menu_id: number;
+  name: string;
+  description: string;
+  diet: string;
+  week_number: number;
+  day_of_week: number;
+}
 
-// Function to fetch menu data from the server
-
-const getMenuData = async () => {
-    const response = await fetch('http://localhost:3000/api/menu');
+const getMenuData = async (): Promise<Menu[]> => {
+  try {
+    const response = await fetch("http://localhost:3000/api/menu");
     const data = await response.json();
     return data;
-  };
+  } catch (error) {
+    console.error("Error fetching menu data:", error);
+    return [];
+  }
+};
 
-  // function to update menu data on the server on the html page
-
-  const updateMenuData = async (data: MenuItem[]) => {
-    const response = await fetch('http://localhost:3000/api/menu', {
-      method: 'PUT',
+const updateMenuData = async (data: Menu): Promise<Menu | null> => {
+  try {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      alert("You must be logged in to update menu data");
+      return null;
+    }
+    const response = await fetch("http://localhost:3000/api/menu/" + data.id, {
+      method: "PUT",
       headers: {
-        'Content-Type': 'application/json',
+        Authorization: "Bearer " + token,
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
     });
     const responseData = await response.json();
     return responseData;
-  };
+  } catch (error) {
+    console.error("Error updating menu data:", error);
+    return null;
+  }
+};
 
-  // Function to display the menus
-  // add input fields for each menu-container to the html page to update the menu
-  // add a button to save the menu
-  
-  const displayMenu = async () => {
-    const menuData = await getMenuData();
-    const menuContainer = document.querySelector('.menu-container');
+const displayMenu = async (): Promise<void> => {
+  const menuData = await getMenuData();
+  const menuContainer = document.querySelector(".menu-container");
+  if (menuContainer) {
+    menuContainer.innerHTML = "";
+  }
+  menuData.forEach((menu) => {
+    console.log(menu);
     if (menuContainer) {
-      menuContainer.innerHTML = '';
+      const menuCard = document.createElement("div");
+      menuCard.classList.add("menu-card");
+      const h3 = document.createElement("h3");
+      h3.innerHTML = menu.name;
+      const p1 = document.createElement("p");
+      p1.innerHTML = menu.description;
+      const p2 = document.createElement("p");
+      p2.innerHTML = menu.diet;
+      const p3 = document.createElement("p");
+      p3.innerHTML = `Menu week: ${menu.week_number}`;
+      const p4 = document.createElement("p");
+      p4.innerHTML = `Menu day: ${menu.day_of_week}`;
+      const form = document.createElement("form");
+      const input1 = document.createElement("input");
+      input1.setAttribute("type", "text");
+      input1.placeholder = "Name";
+      const input2 = document.createElement("input");
+      input2.setAttribute("type", "text");
+      input2.placeholder = "Description";
+      const input3 = document.createElement("input");
+      input3.setAttribute("type", "text");
+      input3.placeholder = "Diet";
+      const button = document.createElement("button");
+      button.setAttribute("type", "submit");
+      button.innerHTML = "Save Menu";
+      menuCard.appendChild(h3);
+      menuCard.appendChild(p1);
+      menuCard.appendChild(p2);
+      menuCard.appendChild(p3);
+      menuCard.appendChild(p4);
+      menuCard.appendChild(form);
+      form.appendChild(input1);
+      form.appendChild(input2);
+      form.appendChild(input3);
+      form.appendChild(button);
+      form.addEventListener("submit", async (event) => {
+        event.preventDefault();
+        const newMenu: Menu = {
+          id: menu.menu_id,
+          name: input1.value,
+          description: input2.value,
+          diet: input3.value,
+          week_number: menu.week_number,
+          day_of_week: menu.day_of_week,
+        };
+        await updateMenuData(newMenu);
+        // menuCard.remove();
+        displayMenu();
+      });
+      menuContainer.appendChild(menuCard);
     }
+  });
+};
 
-    menuData.forEach((menu: MenuItem) => {
-      if (menuContainer) {
-        const menuCard = document.createElement('div');
-        menuCard.classList.add('menu-card');
-        menuCard.innerHTML = `
-          <h3>${menu.name}</h3>
-          <p>${menu.description}</p>
-          <p>${menu.diet}</p>
-          <p>${menu.week_number}</p>
-          <p>${menu.day_of_week}</p>
-          <input type="text" id="menu-name" placeholder="Name" />
-          <input type="text" id="menu-description" placeholder="Description" />
-          <input type="text" id="menu-diet" placeholder="Diet" />
-          <input type="text" id="menu-week-number" placeholder="Week Number" />
-          <input type="text" id="menu-day-of-week" placeholder="Day of Week" />
-          <button id="save-menu">Save Menu</button>
-        `;
-        menuContainer.appendChild(menuCard);
-      }
-    });
-  };
-
-    // Function to save the menus
-const saveMenu = async () => {
-    const menuName = document.querySelector('#menu-name') as HTMLInputElement;
-    const menuDescription = document.querySelector('#menu-description') as HTMLInputElement;
-    const menuDiet = document.querySelector('#menu-diet') as HTMLInputElement;
-    const menuWeekNumber = document.querySelector('#menu-week-number') as HTMLInputElement;
-    const menuDayOfWeek = document.querySelector('#menu-day-of-week') as HTMLInputElement;
+const showMenu = async (): Promise<void> => {
+  const menuName = document.querySelector("#menu-name");
+  const menuDescription = document.querySelector("#menu-description");
+  const menuDiet = document.querySelector("#menu-diet");
+  const menuWeekNumber = document.querySelector("#menu-week-number");
+  const menuDayOfWeek = document.querySelector("#menu-day-of-week");
+  try {
     const menuData = await getMenuData();
-    const newMenu = {
-      id: menuData.length + 1,
-      name: menuName.value,
-      description: menuDescription.value,
-      diet: menuDiet.value,
-      week_number: parseInt(menuWeekNumber.value),
-      day_of_week: parseInt(menuDayOfWeek.value),
-    };
-    menuData.push(newMenu);
-    await updateMenuData(menuData);
+    // const newMenu: Menu = {
+    //   id: menuData.length + 1,
+    //   name: menuName.value,
+    //   description: menuDescription.value,
+    //   diet: menuDiet.value,
+    //   week_number: parseInt(menuWeekNumber.value),
+    //   day_of_week: parseInt(menuDayOfWeek.value),
+    // };
+    // menuData.push(newMenu);
     displayMenu();
-  };
+  } catch (error) {
+    console.error("Error saving menu:", error);
+  }
+};
+
+displayMenu();
