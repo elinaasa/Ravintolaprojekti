08bfb359309bae4d1e8d1380a9e4de01416f21c4
Elina Sankila <sankilaelina@gmail.com>
2023-12-07 10:38:58 +0200

    menu testi

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,14 +1,17 @@
 // Function to fetch menu data from the server
+
 const getMenuData = async () => {
   try {
-    const response = await fetch("http://localhost:3000/api/menu");
+    const response = await fetch('http://localhost:3000/api/menu');
     const data = await response.json();
     return data;
   } catch (error) {
-    console.error("Error fetching menu data:", error);
+    console.error('Error fetching menu data:', error);
     return [];
   }
 };
+
+console.log(getMenuData());
 
 // Function to display the menu inside div with class lunch-items
 // This function is called when the page loads and when the menu is updated
@@ -17,22 +20,22 @@ const getMenuData = async () => {
 
 const displayMenu = async () => {
   const menuData = await getMenuData();
-  const menuContainer = document.querySelector(".lunch-items");
+  const menuContainer = document.querySelector('.lunch-items');
   if (menuContainer) {
-    menuContainer.innerHTML = "";
+    menuContainer.innerHTML = '';
   }
   menuData.forEach((menu) => {
     console.log(menu);
     if (menuContainer) {
-      const lunchItems = document.createElement("div");
-      lunchItems.classList.add("lunch-items");
-      const day = document.createElement("h2");
+      const lunchItems = document.createElement('div');
+      lunchItems.classList.add('lunch-items');
+      const day = document.createElement('h2');
       day.innerHTML = menu.day_of_week;
-      const h2 = document.createElement("h2");
+      const h2 = document.createElement('h2');
       h2.innerHTML = menu.name;
-      const p1 = document.createElement("p");
+      const p1 = document.createElement('p');
       p1.innerHTML = menu.description;
-      const p2 = document.createElement("p");
+      const p2 = document.createElement('p');
       p2.innerHTML = menu.diet;
       lunchItems.appendChild(day);
       lunchItems.appendChild(h2);
