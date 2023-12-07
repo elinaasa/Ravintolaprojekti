89166274605a26809f9068a9508692179f4b1254
eemiljka <eemiljka@metropolia.fi>
2023-12-07 14:15:37 +0200

    login.ts addition

---
+++ Frontend/src/login.ts
@@ -0,0 +1,90 @@
+const loginText: HTMLElement | null = document.querySelector(".title-text .login");
+const loginForm: HTMLFormElement | null = document.querySelector("form.login");
+const loginBtn: HTMLLabelElement | null = document.querySelector("label.login");
+const signupBtn: HTMLLabelElement | null = document.querySelector("label.signup");
+const signupLink: HTMLAnchorElement | null = document.querySelector("form .signup-link a");
+
+if (signupBtn) {
+  signupBtn.onclick = () => {
+    if (loginForm && loginText) {
+      loginForm.style.marginLeft = "-50%";
+      loginText.style.marginLeft = "-50%";
+    }
+  };
+}
+
+if (loginBtn) {
+  loginBtn.onclick = () => {
+    if (loginForm && loginText) {
+      loginForm.style.marginLeft = "0%";
+      loginText.style.marginLeft = "0%";
+    }
+  };
+}
+
+if (signupLink) {
+  signupLink.onclick = (event) => {
+    if (signupBtn) {
+      signupBtn.click();
+      event.preventDefault();
+    }
+  };
+}
+
+document.getElementById("login-icon")?.addEventListener("click", function () {
+  const modalWrapper = document.querySelector<HTMLElement>(".wrapper");
+  if (modalWrapper) {
+    modalWrapper.style.display = "grid";
+    const mainElement = document.querySelector("main");
+    if (mainElement) {
+      mainElement.classList.add("blur");
+    }
+  }
+});
+
+document.getElementById("close-login-modal")?.addEventListener("click", function () {
+  const modalWrapper = document.querySelector<HTMLElement>(".wrapper");
+  if (modalWrapper) {
+    modalWrapper.style.display = "none";
+    const mainElement = document.querySelector("main");
+    if (mainElement) {
+      mainElement.classList.remove("blur");
+    }
+  }
+});
+
+const overlay: HTMLElement | null = document.getElementById("overlay");
+const modalWrapper: HTMLElement | null = document.querySelector(".modal-wrapper");
+
+if (modalWrapper) {
+  modalWrapper.addEventListener("submit", async function (event) {
+    event.preventDefault();
+    const usernameInput: HTMLInputElement | null = document.getElementById("username") as HTMLInputElement;
+    const passwordInput: HTMLInputElement | null = document.getElementById("password") as HTMLInputElement;
+
+    if (usernameInput && passwordInput) {
+      const username = usernameInput.value;
+      const password = passwordInput.value;
+      const loginData = { username, password };
+
+      const response = await fetch("http://localhost:3000/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(loginData),
+      });
+
+      const data = await response.json();
+      console.log(data);
+
+      if (data.token) {
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("user", JSON.stringify(data.user));
+        window.location.href = "http://127.0.0.1:5501/Frontend/admin.html";
+      } else {
+        alert("Login failed");
+      }
+    }
+  });
+}
