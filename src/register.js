b88a9eb2527a9988c55ad73c3fbdb5be04e462bd
MetroHege <113589548+MetroHege@users.noreply.github.com>
2023-12-13 11:16:07 +0200

    -s

--- src/register.js
+++ src/register.js
@@ -11,7 +11,7 @@ document
 
     try {
       const response = await fetch(
-        'https://ucad.northeurope.cloudapp.azure.com/api/api/users',
+        'https://ucad.northeurope.cloudapp.azure.com/api/api/user',
         {
           method: 'POST',
           headers: {
